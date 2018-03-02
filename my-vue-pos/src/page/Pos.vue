70ae0e532dcc098b4d490b5829bbbdbe1a4d2d77
wupuyi <wupuyi@foxmail.com>
2018-03-02 23:30:25 +0800

    前置工作完成

---
+++ my-vue-pos/src/page/Pos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pos">
+    Hello Pos Demo!
+    <i class="iconfont icon-dianpu"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pos'
+}
+</script>
+
+<style>
+  .icon-dianpu {
+    font-size: 50px
+  }
+</style>
